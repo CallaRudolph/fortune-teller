c9a51b326ffcb88740cb744abb3e7d55b97d8e51
Calla Rudolph and Max Scher <max@>
2017-06-06 14:56:11 -0700

    add functions to count checked inputs, create cher if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
-  
+  $(".zolfar-Qs").submit(function(event) {
+    event.preventDefault();
 
+    var counterFood = 0;
+    $("input:checkbox[name=food]:checked").each(function() {
+      var selection = parseInt($(this).val());
+      counterFood += selection;
+    });
+    var counterAct = 0;
+    $("input:checkbox[name=act]:checked").each(function(){
+      var selection = parseInt($(this).val());
+      counterAct += selection;
+    });
+    var counterBio = 0;
+    $("input:checkbox[name=bio]:checked").each(function(){
+      var selection = parseInt($(this).val());
+      counterBio += selection;
+    });
+
+    $(".future").hide();
+    if ($("input:checkbox[id=cher]:checked")) {
+      $("#immortal").show();
+    }
+  });
 });
